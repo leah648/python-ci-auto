e4a570212e57ef6d690b5bcf0d2802c53e1e0c6d
Leah Moshe <moshelea123@gmail.com>
2026-01-21 21:01:12 +0200

    Thta's fun!!!

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,16 +2,16 @@ import pytest
 from services.calculator_service import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 88
+    assert add(2, 3) == 0
 
 def test_subtract():
-    assert subtract(5, 3) == 555
+    assert subtract(5, 3) == 0
 
 def test_multiply():
-    assert multiply(4, 3) == 12
+    assert multiply(4, 3) == 0
 
 def test_divide():
-    assert divide(10, 2) == 3
+    assert divide(10, 2) == 0
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
